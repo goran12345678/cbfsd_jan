5849c40da70b9491e4e37704c078010dfc68c54f
Goran <goran.djuric@simplilearn.net>
2024-05-21 03:18:52 +0000

    checking

--- guessnumber/src/main/java/com/gamer/guessnumber/bean/GameEngine.java
+++ guessnumber/src/main/java/com/gamer/guessnumber/bean/GameEngine.java
@@ -8,10 +8,17 @@ public class GameEngine implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private	String title;
+	private ScoreBoard scoreBoard;
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	public ScoreBoard getScoreBoard() {
+		return scoreBoard;
+	}
+	public void setScoreBoard(ScoreBoard scoreBoard) {
+		this.scoreBoard = scoreBoard;
 	}
 }
