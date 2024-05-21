67f88445f7e47c207b4e5dc29919643d858a7660
Goran <goran.djuric@simplilearn.net>
2024-05-21 03:07:30 +0000

    checkin

---
+++ guessnumber/src/main/java/com/gamer/guessnumber/App.java
@@ -0,0 +1,31 @@
+package com.gamer.guessnumber;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.gamer.guessnumber.bean.GameEngine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        ApplicationContext context = 
+        		new ClassPathXmlApplicationContext("context.xml");
+        
+        GameEngine engine = (GameEngine) context.getBean("engine");
+        
+        GameEngine engine2 = (GameEngine) context.getBean("engine");
+        engine2.setTitle("My new title");
+        
+        System.out.println("Game engine title: " + engine.getTitle());
+        
+        
+        
+    }
+}
